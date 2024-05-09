059c2cecb1eb004ffe015722e63bfc01029980c0
DavidLineman <dline215@gmail.com>
2024-05-09 15:01:00 -0400

    added flash messages to create, update and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,10 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(:article_params)
+        @article = Article.new(article_params)
         @article.save
+
+        flash.notice = "Article '#{@article.title}' has been Created!"
 
         redirect_to article_path(@article)
     end 
@@ -23,8 +25,23 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         @article.destroy
 
+        flash.notice = "Article '#{@article.title}' has been Deleted!"
+
         redirect_to articles_url
     end 
+
+    def edit 
+        @article = Article.find(params[:id])
+    end 
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title} Updated!"
+
+        redirect_to article_path(@article)
+    end
 
     private 
 
